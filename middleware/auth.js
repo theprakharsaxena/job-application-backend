9626f219f96768f117e5b87169af27370d025140
Prakhar Saxena <prakharsaxena5125@gmail.com>
2024-04-07 11:08:10 +0530

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,15 @@
-import { catchAsyncError } from "./catchAsyncError.js"
-import ErrorHandler from "./error.js"
-import jwt from "jsonwebtoken"
-import { User } from "../models/userSchema.js"
+import { catchAsyncError } from "./catchAsyncError.js";
+import ErrorHandler from "./error.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/userSchema.js";
 
-export const isAuthorized  = catchAsyncError(async(req,res,next)=>{
-    const {token} = req.cookies
-    if(!token){
-        return  next(new ErrorHandler("user not authorized",400))
-    }
-    const decoded = jwt.verify(token,process.env.JWT_SECRET_KEY)
-    req.user = await User.findById(decoded.id)
-    next()
-})
+export const isAuthorized = catchAsyncError(async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return next(new ErrorHandler("user not authorized", 400));
+  }
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  console.log(jwt.verify(token, process.env.JWT_SECRET_KEY));
+  req.user = await User.findById(decoded.id);
+  next();
+});
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,16 @@
-import express  from "express";
-import { register,login, logout, getUser } from "../controllers/userController.js";
+import express from "express";
+import {
+  register,
+  login,
+  logout,
+  getUser,
+} from "../controllers/userController.js";
 import { isAuthorized } from "../middleware/auth.js";
 
-const router = express.Router()
-router.post("/register",register)
-router.post("/login",login)
-router.get("/logout",isAuthorized,logout)
-router.get("/getuser",isAuthorized,getUser)
+const router = express.Router();
+router.post("/register", register);
+router.post("/login", login);
+router.get("/logout", logout);
+router.get("/getuser", isAuthorized, getUser);
 
 export default router;
